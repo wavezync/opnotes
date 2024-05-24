9b61c9c29bd96fed0494486e117cb99eeff751ab
Kasun Vithanage <alankasun@gmail.com>
2024-05-24 12:09:15 +0530

    feat: electron logger

--- src/main/index.ts
+++ src/main/index.ts
@@ -12,6 +12,9 @@ export function getAutoUpdater(): AppUpdater {
   // Using destructuring to access autoUpdater due to the CommonJS module of 'electron-updater'.
   // It is a workaround for ESM compatibility issues, see https://github.com/electron-userland/electron-builder/issues/7976.
   const { autoUpdater } = electronUpdater
+  const log = require('electron-log')
+  log.transports.file.level = 'debug'
+  autoUpdater.logger = log
   return autoUpdater
 }
 
