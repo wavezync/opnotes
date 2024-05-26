a32f2f0da24f0b5901f8f73a83caa9e8e4a61e3c
Kasun Vithanage <alankasun@gmail.com>
2024-05-26 21:21:07 +0530

    chore: remove unused

--- src/renderer/src/components/common/AutoCompleteInput.tsx
+++ src/renderer/src/components/common/AutoCompleteInput.tsx
@@ -1,4 +1,4 @@
-import { Check, Plus, PlusIcon } from 'lucide-react'
+import { Check, PlusIcon } from 'lucide-react'
 
 import { cn } from '@/lib/utils'
 import { Button } from '@/components/ui/button'
